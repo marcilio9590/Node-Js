c04e0003cada52843f03e21d7cf643b8e0e8b5b0
Marcilio Cobel <marcilio.c.junior@accenture.com>
2019-03-08 20:46:13 -0300

    crud

--- Curso/src/controllers/product-controller.js
+++ Curso/src/controllers/product-controller.js
@@ -3,6 +3,16 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+    Product.find({
+        active: true
+    }, 'title price slug').then(data => {
+        res.status(200).send(data);
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+}
+
 exports.post = (req, res, next) => {
     var product = new Product();
     product.title = req.body.title;
@@ -11,20 +21,75 @@ exports.post = (req, res, next) => {
     product.price = req.body.price;
     product.tags = req.body.tags;
     product.save().then(x => {
-        res.status(201).send({ message: 'Produto Cadastrado com Sucesso.' });
+        res.status(201).send({
+            message: 'Produto Cadastrado com Sucesso.'
+        });
     }).catch(e => {
-        res.status(400).send({ message: 'Falha ao Cadastrar Produto.', data: e });
+        res.status(400).send({
+            message: 'Falha ao Cadastrar Produto.', data: e
+        });
     });
 }
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
+    Product.findByIdAndUpdate(req.params.id, {
+        $set: {
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price
+        }
+    }).then(x => {
+        res.status(201).send({
+            message: 'Produto Atualizado com Sucesso.'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao Atualizar Produto.', data: e
+        });
     });
 };
 
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
+    Product.findByIdAndRemove(req.body.id).then(x => {
+        res.status(201).send({
+            message: 'Produto Removido com Sucesso.'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao Remover Produto.', data: e
+        });
+    });
 };
+
+exports.getBySlug = (req, res, next) => {
+    Product.findOne({
+        slug: req.params.slug,
+        active: true
+    }, 'title description price slug tags').then(data => {
+        res.status(200).send(data);
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+}
+
+exports.getById = (req, res, next) => {
+    Product.findOne({
+        _id: req.params.id
+    }).then(data => {
+        res.status(200).send(data);
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+}
+
+exports.getByTag = (req, res, next) => {
+    Product.find({
+        tags: req.params.tag,
+        active: true
+    }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
--- Curso/src/routes/product-route.js
+++ Curso/src/routes/product-route.js
@@ -5,7 +5,11 @@ const router = express.Router();
 const controller = require('../controllers/product-controller');
 
 router.post('/', controller.post);
-router.delete('/', controller.put);
-router.put('/:id', controller.delete);
+router.delete('/', controller.delete);
+router.put('/:id', controller.put);
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
+router.get('/admin/:id', controller.getById);
+router.get('/tags/:tag', controller.getByTag);
 
 module.exports = router;
